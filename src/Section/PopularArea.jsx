ae091ba152e8eae5241db424783f937a88b98f60
Maaz Tai <githubmaaz@gmail.com>
2025-05-24 09:29:52 +0530

    Update PopularArea.jsx

--- src/Section/PopularArea.jsx
+++ src/Section/PopularArea.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import area1 from "../assets/area1.jpg";
 import area2 from "../assets/area2.jpg";
 import area3 from "../assets/area3.jpg";
-import { useEffect } from "react";
 
 const PopularArea = () => {
   useEffect(() => {
@@ -17,12 +16,14 @@ const PopularArea = () => {
   }, []);
 
   return (
-    <div className="">
-      <section id="popularareas" className="lg:w-[90%] w-full m-auto bg-cover bg-red-200 bg-center rounded-xl flex justify-center mt-10 relative flex-col lg:px-20  px-6  py-20 gap-20">
-        <div
-          id="top"
-          className="w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8"
-        >
+    <div>
+      <section
+        id="popularareas"
+        className="w-full m-auto bg-cover bg-red-200 bg-center rounded-xl flex justify-center mt-10 relative flex-col px-4 sm:px-6 lg:px-20 py-20 gap-20"
+      >
+        {/* Top Section */}
+        <div className="w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8">
+          {/* Heading */}
           <div>
             <h1
               data-aos="zoom-in"
@@ -40,64 +41,38 @@ const PopularArea = () => {
               Popular Areas
             </h1>
           </div>
-          <div className="grid lg:grid-cols-3 col-span-2 justify-center items-center gap-6">
-            <div
-              className="h-[300px] w-[90%] sm:w-[300px] bg-cover bg-center rounded-xl"
-              data-aos="zoom-in"
-              data-aos-delay="300"
-              style={{ backgroundImage: `url(${area1})` }}
-            ></div>
-            <div
-              className="h-[300px] w-[90%] sm:w-[300px] bg-cover bg-center rounded-xl"
-              data-aos="zoom-in"
-              data-aos-delay="400"
-              style={{ backgroundImage: `url(${area2})` }}
-            ></div>
-            <div
-              className="h-[300px] w-[90%] sm:w-[300px] bg-cover bg-center rounded-xl"
-              data-aos="zoom-in"
-              data-aos-delay="300"
-              style={{ backgroundImage: `url(${area3})` }}
-            ></div>
+
+          {/* Image Cards */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full justify-items-center col-span-2">
+            {[area1, area2, area3].map((img, index) => (
+              <div
+                key={index}
+                className="h-[300px] w-full max-w-[300px] bg-cover bg-center rounded-xl"
+                style={{ backgroundImage: `url(${img})` }}
+                data-aos="zoom-in"
+                data-aos-delay={300 + index * 100}
+              />
+            ))}
           </div>
         </div>
 
-        <div
-          id="bottom"
-          className="w-full grid lg:grid-cols-3 grid-cols-1 lg:justify-center justify-start items-start gap-6"
-        >
-          <div
-            data-aos="slide-up"
-            data-aos-delay="200"
-            className="flex justify-center lg:items-center gap-8 w-full"
-          >
-            <h1 className="text-black text-7xl font-semibold leading-10 ">
-              5K
-            </h1>
-            <h1>Active <br/>Listings</h1>
-          </div>
-          <div
-            data-aos="slide-up"
-            data-aos-delay="200"
-            className="flex justify-center lg:items-center gap-8 w-full"
-          >
-            <h1 className="text-black text-7xl font-semibold leading-10 ">
-              5K
-            </h1>
-            <h1>Active <br/>Listings</h1>
-            
-          </div>
-          <div
-            data-aos="slide-up"
-            data-aos-delay="200"
-            className="flex justify-center lg:items-center gap-8 w-full"
-          >
-            <h1 className="text-black text-7xl font-semibold leading-10 ">
-              5K
-            </h1>
-            <h1>Active <br/>Listings</h1>
-            
-          </div>
+        {/* Bottom Counters */}
+        <div className="w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-start gap-6">
+          {[...Array(3)].map((_, i) => (
+            <div
+              key={i}
+              data-aos="slide-up"
+              data-aos-delay="200"
+              className="flex justify-center items-center gap-8 w-full"
+            >
+              <h1 className="text-black text-7xl font-semibold leading-10">
+                5K
+              </h1>
+              <h1>
+                Active <br /> Listings
+              </h1>
+            </div>
+          ))}
         </div>
       </section>
     </div>
